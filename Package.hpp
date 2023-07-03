c6f4a69cf60ba5f45dccd2dd081864e18d9af213
carloronconi <carlo.ronconi@mail.polimi.it>
2023-07-03 15:53:26 +0200

    Refactor Package for better code readability

--- Package.hpp
+++ Package.hpp
@@ -18,25 +18,29 @@ enum State {held, falling, ground};
 
 class Package {
 private:
+    // constants
+    // rotation and motion speed
+    const float MAX_SPEED = 15.0;
+    // plane physics parameters
+    const float SCALE = 0.1;
+    const float TARGET_PRECISION = 10.0;
+    // friction deceleration in object coordinates
+    const vec3 FRICTION = vec3(5, 1, 5); // stronger horizontal friction makes dropping easier
+
     // state of the plane in world coordinates
     vec3 position;
     vec3 speed{0, 0, 0};
     vec3 externalAccelerations{0, - 3, 0};
     // e.g. gravity only would be (0, -9.81, 0)
     // e.g. gravity plus x-wind would be (2.5, -9.81, 0)
-    mat3 uAxes;
-
-    // friction deceleration in object coordinates
-    vec3 planeCoordinatesFriction{5, 1, 1};
+    // uAxes are actually fixed here, because we don't need to keep track of the package rotation
+    mat3 uAxes = mat4(1, 0, 0, 1,
+                      0, 1, 0, 1,
+                      0, 0, 1, 1,
+                      0, 0, 0, 1);
 
     // reference to command inputs used to update the world matrix
     UserInputs* inputs;
-
-    // constants
-    // rotation and motion speed
-    const float MAX_SPEED = 10.0f;
-    // plane physics parameters
-    const float PLANE_SCALE = 0.1f;
 
     // package specific
     const glm::vec3& planePosition;
@@ -44,16 +48,6 @@ private:
     const glm::vec3& targetPosition;
     bool hitTarget = false;
     State state;
-    const float TARGET_PRECISION = 10.0f;
-
-    void updateUAxes() {
-        // unitary-length axes xyz in plane coordinate rotated to xyz axes in world space
-        // used to pass from plane's coordinate system to world coordinate system
-        uAxes = mat4(1, 0, 0, 1,
-                     0, 1, 0, 1,
-                     0, 0, 1, 1,
-                     0, 0, 0, 1);
-    }
 
 public:
     Package(const vec3 &planePosition, const vec3 &planeSpeed, const vec3&  targetPosition) :
@@ -71,7 +65,6 @@ public:
      * @return updated world matrix
      */
     mat4 computeWorldMatrix() {
-
         switch (state) {
             case held:
                 hitTarget = false;
@@ -82,17 +75,15 @@ public:
                 } else {
                     position = {0, -2, 0};
                     speed = {0, 0, 0};
-                    return translate(mat4(1), position) * glm::scale(glm::mat4(1), glm::vec3(PLANE_SCALE));
+                    return translate(mat4(1), position) * glm::scale(glm::mat4(1), glm::vec3(SCALE));
                 }
             case falling: {
-                updateUAxes();
-
                 speed += inputs->deltaT * externalAccelerations; // external accelerations (doesn't require multiplying by uAxes: already in world coordinates)
 
                 // friction deceleration and speed limiting are computed in plane space
                 vec3 planeSpeed = inverse(uAxes) * speed; // convert speed from world to plane space
                 // plane speed is reduced by acceleration coordinates times deltaT in the opposite direction of plane speed
-                planeSpeed -= planeCoordinatesFriction * inputs->deltaT * normalize(planeSpeed);
+                planeSpeed -= FRICTION * inputs->deltaT * normalize(planeSpeed);
                 // plane speed magnitude (misleadingly named glm::length) is capped at max speed by multiplying the scalar for the direction of plane speed
                 if (glm::length(planeSpeed) > MAX_SPEED) planeSpeed = MAX_SPEED * normalize(planeSpeed);
                 speed = uAxes * planeSpeed; // update world speed converting back from plane speed
@@ -105,7 +96,7 @@ public:
 
                     state = ground;
                 } else {
-                    return translate(mat4(1), position) * glm::scale(glm::mat4(1), glm::vec3(PLANE_SCALE));
+                    return translate(mat4(1), position) * glm::scale(glm::mat4(1), glm::vec3(SCALE));
                 }
             }
             case ground:
@@ -117,7 +108,7 @@ public:
                 } else {
                     cout << "\nTarget missed :(\n";
                 }
-                return translate(mat4(1), position) * glm::scale(glm::mat4(1), glm::vec3(PLANE_SCALE));
+                return translate(mat4(1), position) * glm::scale(glm::mat4(1), glm::vec3(SCALE));
         }
     }
 
